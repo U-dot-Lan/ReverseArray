29accc694d734d367c35e9cc6266446c315d26b6
Ulanbek Bolotbekov <drreal@mail.ru>
2021-12-29 20:57:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = new string[] { "h", "e", "l", "l", "o" };
+            RA.ReverseArray(str);
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.Write(str[i]);
+            }
+        }
+    }
+}
---
+++ RA.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseArray
+{
+    public static class RA
+    {
+        public static void ReverseArray(string[] str)
+        {
+            int length = (str.Length + 1) / 2;
+            for (int i = 0, j = str.Length - 1; i < length;  i++, j--)
+            {
+                string temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
